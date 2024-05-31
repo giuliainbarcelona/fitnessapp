b6356a3fec177218fdab39b79c685358ed9a5a25
Chardae Schnabel <chardaeschnabel@gmail.com>
2024-05-31 15:41:17 +0200

    drawdown funciton complete for muscles

--- client/src/pages/Buildyourworkout.jsx
+++ client/src/pages/Buildyourworkout.jsx
@@ -1,29 +1,96 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const apiurl = "https://api-ninjas.com/api/exercises";
 
 export default function buildyourowkworkout() {
-  const [workout, setWorkout] = useState([]);
+  const [inputCriteria, setInputCriteria] = useState({
+    muscle: "",
+    type: "",
+    difficulty: "",
+  });
+  const [workouts, setWorkouts] = useState([]);
+  const muscles = [
+    "abdominals",
+    "abductors",
+    "adductors",
+    "biceps",
+    "calves",
+    "chest",
+    "forearms",
+    "glutes",
+    "hamstrings",
+    "lats",
+    "lower_back",
+    "middle_back",
+    "neck",
+    "quadriceps",
+    "traps",
+    "triceps",
+  ];
 
-  async function Submission() {
+  const difficulty = ["beginner", "intermediate", "expert"];
+
+  const type = [
+    "cardio",
+    "olympic_weightlifting",
+    "plyometrics",
+    "powerlifting",
+    "strength",
+    "stretching",
+    "strongman",
+  ];
+
+  async function getWorkout() {
     try {
-      const options = await fetch(`https://api.api-ninjas.com/v1/exercises?muscle=${muscle}&difficulty=${difficulty}`, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-        "X-Api-Key": "TAV8D89aex3FxVlNTvqVtA==DPoPSnNYBCrqU9ZY",
-      });
+      const options = await fetch(
+        `https://api.api-ninjas.com/v1/exercises?muscle=${inputCriteria.muscle}&difficulty=${inputCriteria.difficulty}&type=${inputCriteria.type}`,
+        {
+          method: "GET",
+          headers: { "Content-Type": "application/json" },
+          "X-Api-Key": "TAV8D89aex3FxVlNTvqVtA==DPoPSnNYBCrqU9ZY",
+        }
+      );
       if (!response.ok) {
-        throw new Error ("Network response not ok")
-      };
+        throw new Error("Network response not ok");
+      }
       const exerciseList = await options.json();
-      setWorkout(exerciseList);
+      setWorkouts(exerciseList);
     } catch (err) {
       res.status(500).send(err);
       console.log("Error message here", err);
     }
   }
 
-  return(
-     <div>workout builder let's see</div>
-    )
+  return (
+    <>
+      <select name="muscle" value={inputCriteria.muscle}>
+        {muscles.map((muscle, idx) => (
+          <option key={idx} value={muscle}>
+            {muscle}
+          </option>
+        ))}
+      </select>
+      {/* <select name="type" value={workout.type}>
+        {workouts.map((workout) => (
+          <option key={workout.id} value={workout.id}>
+            {type}
+          </option>
+        ))}
+      </select> */}
+      {/* <select name="difficulty" value={workout.difficulty}>
+        <option></option>
+      </select> */}
+      <div>workout builder let's see</div>
+    </>
+  );
+}
+
+{
+  /* <select name="Next_1" value={step.Next_1}>
+  {allSteps.map((step) => (
+    <option key={step.id} value={step.id}>
+      {step.Description}
+    </option>
+  ))}
+</select>; */
 }
