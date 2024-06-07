fc15a2fcf4289641cc9ebf840ab61d38165156fc
Chardae Schnabel <chardaeschnabel@gmail.com>
2024-06-07 16:30:09 +0200

    search query

--- routes/users.js
+++ routes/users.js
@@ -9,23 +9,7 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
-
-router.get("/:user_id", userShouldExist, async function (req, res, next) {
-  try {
-    const userId = req.params.user_id;
-    //returns user information with a specific user id
-    const userQuery = `SELECT * FROM users WHERE id = ${userId}`;
-    const result = await db(userQuery);
-    if (result.data.length) {
-      res.status(200).json(result.data[0]);
-    }
-  } catch (error) {
-    console.error("Error fetching user:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-//unfinished, working on it 
+//unfinished, working on it
 router.get("/search", async function (req, res, next) {
   try {
     const { q } = req.query;
@@ -40,6 +24,21 @@ router.get("/search", async function (req, res, next) {
     res.status(200).json(result.data);
   } catch (error) {
     console.error("Error searching for users:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+router.get("/:user_id", userShouldExist, async function (req, res, next) {
+  try {
+    const userId = req.params.user_id;
+    //returns user information with a specific user id
+    const userQuery = `SELECT * FROM users WHERE id = ${userId}`;
+    const result = await db(userQuery);
+    if (result.data.length) {
+      res.status(200).json(result.data[0]);
+    }
+  } catch (error) {
+    console.error("Error fetching user:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 });
