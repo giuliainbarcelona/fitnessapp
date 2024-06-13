c3635c26c2c81349aa41788ee794eb9ce232c4f6
Giulia Cellerino <giulia.cellerino@icloud.com>
2024-06-13 12:12:13 +0200

    Navbar username rendering

--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -1,22 +1,30 @@
 import { createContext, useState, useContext } from "react";
-
+import { useEffect } from "react";
 const AuthContext = createContext();
-
 const AuthProvider = (props) => {
   const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem("token"));
-  const [currentUser, setCurrentUser] = useState({});
-
+  const [currentUser, setCurrentUser] = useState(() => {
+    const storedUser = localStorage.getItem("currentUser");
+    return storedUser ? JSON.parse(storedUser) : {};
+  });
   const signIn = (user) => {
     console.log("LOGIN");
     setIsLoggedIn(true);
     setCurrentUser(user);
+    localStorage.setItem("currentUser", JSON.stringify(user));
   };
-
   const signOut = () => {
     setIsLoggedIn(false);
     setCurrentUser({});
+    localStorage.removeItem("currentUser");
+    localStorage.removeItem("token");
   };
-
+  useEffect(() => {
+    const storedUser = localStorage.getItem("currentUser");
+    if (storedUser) {
+      setCurrentUser(JSON.parse(storedUser));
+    }
+  }, []);
   return (
     <AuthContext.Provider
       value={{
@@ -29,7 +37,5 @@ const AuthProvider = (props) => {
     />
   );
 };
-
 const useAuth = () => useContext(AuthContext);
-
 export { AuthProvider, useAuth, AuthContext };
