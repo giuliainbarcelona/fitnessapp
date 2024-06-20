fa8d5b16d3dad90066101e007d0584005a346a28
Giulia Cellerino <giulia.cellerino@icloud.com>
2024-06-20 11:35:04 +0200

    Readme file updates

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,6 @@ const db = require("../model/helper");
 const userShouldExist = require("../guards/userShouldExist");
 const userShouldBeLoggedIn = require("../guards/userShouldBeLoggedIn");
 
-
-
-
-
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
@@ -62,6 +58,5 @@ router.get(
     }
   }
 );
-
 
 module.exports = router;
